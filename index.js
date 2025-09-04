39942890096783c8324fbba13ddbb85ff3e3f60f
Axel-Wolkowicz <wolkowiczaxel@gmail.com>
2025-09-04 11:26:03 -0300

    Terminado Wally

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ import {generarEspacio} from "./busqueda.js";
 let lista = generarEspacio(100);
 console.log (lista);
 for (let i = 0; i < lista.length; i++){
-    let posicionLista = lista [i];
-    if (posicionLista === "Wally"){
+    let nombre = lista [i];
+    if (nombre === "Wally"){
         console.log ("Wally esta en la posición " + i);
+    }
+}
+for (let i=0; i < lista.length; i++){
+    let primeraLetra = lista[i].charAt(0);
+    if (primeraLetra === "W"){
+        console.log ("Arranca por W el nombre en la posición " + i);
+    }
+}
+for (let i = 0; i < lista.length; i++){
+    let dobleEle = "ll";
+    if (lista[i].includes(dobleEle)){
+        console.log("Doble L en la posición " + i);
     }
 }
